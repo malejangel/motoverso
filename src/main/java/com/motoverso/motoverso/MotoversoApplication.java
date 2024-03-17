cedeea0e2ddb1d7cdda98c81dddad7cb5d228067
aleja_s3fxgqk <alejandri24@gmail.com>
2024-03-16 22:10:00 -0500

    mi primer commit

---
+++ src/main/java/com/motoverso/motoverso/MotoversoApplication.java
@@ -0,0 +1,13 @@
+package com.motoverso.motoverso;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MotoversoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MotoversoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/motoverso/motoverso/MotoversoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.motoverso.motoverso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MotoversoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
